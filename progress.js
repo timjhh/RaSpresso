b8d75b02ad2aa03f448d121a07da9a3da55b987e
Tim Harrold <timjharrold@gmail.com>
2021-11-23 14:02:39 -0500

    Updated Refs, Added d3 arc

--- progress.js
+++ progress.js
@@ -1,18 +1,49 @@
 $(document).ready(function() {
 
-var dataset = [1,2,3];
+const margin = {top: 20, bottom: 20, left: 30, right: 30},
+height =500-margin.top-margin.bottom,
+width=500-margin.left-margin.right;
+
+
+var data = [],
+i = 0;
+
+for(i=0; i<5; i++){
+    data.push(Math.round(Math.random()*100));
+}
+
+var arc = d3.arc()
+  .innerRadius(80)
+  .outerRadius(100)
+  .cornerRadius(20);
+
+var pie = d3.pie()
+  .sort(null)
+  .value(function(d) {
+    return d;
+  });
+
+
 
 var svg = d3.select("#pChart")
-    .append("svg");
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .append("g")
+    .attr("transform", "translate(" + (width/2 +margin.right) + "," + (height/2) + ")");
 
-svg.selectAll("circle")
-    .data(dataset)
-    .enter().append("circle")
-    .style("stroke", "gray")
-    .style("fill", "black")
-    .attr("r", 40)
-    .attr("cx", 50)
-    .attr("cy", 20);
+
+
+svg.selectAll(".arc")
+  .data(pie(data))
+  .enter()
+  .append("path")
+  .attr("class", "arc")
+  .style("fill", function(d) {
+    return "red";
+  })
+  .attr("d", arc);
+
 
 
 
