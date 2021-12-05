8a40be6fee89351b06b84dc815359c89ed59774e
Quimby Warden <qwarden@uvm.edu>
2021-12-05 17:59:03 -0500

    Added temperature reading functionality

--- app/app.py
+++ app/app.py
@@ -1,5 +1,8 @@
 from flask import *
 import RPi.GPIO as GPIO
+import os
+import glob
+import time
 
 
 app = Flask(__name__)
@@ -9,15 +12,12 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 # define pins to use
-TEMP_PIN = 18
 RING_PIN = 23
 
 # set up pins
 #   TEMP_PIN takes IN temperature sensor data
 #   RING_PIN sends OUT information to the LED progress ring
-GPIO.setup(TEMP_PIN, GPIO.IN)
 GPIO.setup(RING_PIN, GPIO.OUT)
-
 
 
 # run when start button is pressed
@@ -32,18 +32,44 @@ def cook():
     # let us know the process is complete
     return "cook process complete"
 
+
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+
+# get temperature data file
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28*')[0]
+device_file = device_folder + '/w1_slave'
+
+
+def read_temp_raw():
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+
+
 @app.route('/temp', methods=['GET'])
 def temp():
-    tempNum = 220;
-    return str(tempNum);
-    
+    lines = read_temp_raw()
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temp_raw()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+        temp_f = temp_c * 9.0 / 5.0 + 32.0
+        print(temp_f)
+        return str(temp_f)
+
 
 @app.route('/', methods=['GET'])
 def default():
-    ### TODO Uncomment and send data to webpage if route fails
-    #temp = GPIO.input(/////)
-    #tmpData = {
+    # TODO Uncomment and send data to webpage if route fails
+    # temp = GPIO.input(/////)
+    # tmpData = {
     #    'temp': temp
-    #}
-    #return render_template("index.html", **tmpData)
+    # }
+    # return render_template("index.html", **tmpData)
     return render_template("index.html", **{'temp': 48})
