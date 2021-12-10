24fd3d044d08ad0a375c2dee71948c89853feeef
Tim Harrold <timjharrold@gmail.com>
2021-12-09 23:13:29 -0500

    Culled Files

--- temp_data_create_table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `temperature_data` (
-  `id` int(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `timestamp` varchar(50) NOT NULL,
-  `value_c` float NOT NULL,
-  `value_f` float NOT NULL
-);
